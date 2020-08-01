6b2493c108ca4475055f406c21b3be203e6c4d76
Cristina <cristinaleca9@gmail.com>
2020-08-01 12:43:55 -0400

    add 2 messages GitPractice.lava

--- src/GitPractice.java
+++ src/GitPractice.java
@@ -5,5 +5,8 @@ public class GitPractice {
 
         // add 2 more lines and remove the previous message
 
+        // add 2 messages
+        System.out.println("I like today's class");
+        System.out.println("Asya is a great teacher");
     }
 }
